6beb49b17a6827715904d12251fdeb4bcb31f99f
beluCorvalan <bcorvalan.amil@gmail.com>
2021-06-24 18:52:53 -0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {CartProvider} from './CartContex';
+import Product from './component/Product'
+import CartContent from './component/CartContent'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CartProvider>
+      <Product title="Remera azul" description="remera linda" id= {1}/>
+      <Product title="pantalon negro" description="pantalon linda" id= {2}/>
+      <CartContent/>
+    </CartProvider>
+    <UseMemo/>
+    </>
   );
 }
 
